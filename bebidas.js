49a62e5a8b54a064f8b5b43cace7a84931e4120b
Otávio Roschel <157381939+otavioroschel@users.noreply.github.com>
2025-03-19 22:18:47 -0300

    cc

--- bebidas.js
+++ bebidas.js
@@ -1,12 +1,12 @@
 const bebidas = [
-    { nome: "Coca-Cola 2L", preco: "R$ 16,00", imagem: "../css/assets/coca2l.png" },
-    { nome: "Pepsi 2L", preco: "R$ 15,00", imagem: "../css/assets/pepsi2l.webp" },
-    { nome: "Guaraná Antartica 2L", preco: "R$ 14,00", imagem: "../css/assets/guaranaantartica.jpg" },
-    { nome: "Fanta Laranja 2L", preco: "R$ 14,00", imagem: "../css/assets/fanta2l.png" },
-    { nome: "Sprite 2L", preco: "R$ 14,00", imagem: "../css/assets/sprite.jpg" },
-    { nome: "H2OH! 1,5L", preco: "R$ 12,00", imagem: "../css/assets/h20h.webp" },
-    { nome: "Schweppes 1,5L", preco: "R$ 12,00", imagem: "../css/assets/citrus.webp" },
-    { nome: "Guaraná Jesus 2L", preco: "R$ 12,00", imagem: "../css/assets/guaranajesus.webp" },
+    { nome: "Coca-Cola 2L", preco: "R$ 16,00", imagem: "./css/assets/coca2l.png" },
+    { nome: "Pepsi 2L", preco: "R$ 15,00", imagem: "./css/assets/pepsi2l.webp" },
+    { nome: "Guaraná Antartica 2L", preco: "R$ 14,00", imagem: "./css/assets/guaranaantartica.jpg" },
+    { nome: "Fanta Laranja 2L", preco: "R$ 14,00", imagem: "./css/assets/fanta2l.png" },
+    { nome: "Sprite 2L", preco: "R$ 14,00", imagem: "./css/assets/sprite.jpg" },
+    { nome: "H2OH! 1,5L", preco: "R$ 12,00", imagem: "./css/assets/h20h.webp" },
+    { nome: "Schweppes 1,5L", preco: "R$ 12,00", imagem: "./css/assets/citrus.webp" },
+    { nome: "Guaraná Jesus 2L", preco: "R$ 12,00", imagem: "./css/assets/guaranajesus.webp" },
 ];
 
 const cardBebidas = document.querySelector(".cardbebidas");
